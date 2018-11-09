9acffe8cc7311bd015889143ac56a0a3fd683892
Edwin Didier Méndez Rojas - Ceiba Software <edwin.mendez@ceiba.com.co>
2018-11-09 16:11:30 -0500

    e

--- featureFiles/IngresoVehiculoParqueadero.feature
+++ featureFiles/IngresoVehiculoParqueadero.feature
@@ -3,7 +3,7 @@ Feature: prueba de ingreso vehiculos al parqueadero
   Quiero ingresar a la pagina web del parqueadero
   Para poder ingresar vehiculos al parqueadero
 
-  Scenario Outline: Title of your scenario outline
+  Scenario Outline: ingresar vehiculo tipo moto
     Given el vigilante esta en la pagina principal del parqueadero
     When el vigilante de click en el tab ingresar vehiculo
     And este en el tab moto
@@ -15,3 +15,15 @@ Feature: prueba de ingreso vehiculos al parqueadero
     Examples: 
       | placa   | cilindraje | mensaje                                 |
       | 888-ccc |       3000 | Moto con placa 888-ccc Parqueado Exito! |
+
+  Scenario Outline: ingresar vehiculo tipo carro
+    Given el vigilante se encuentra la pagina principal del parqueadero
+    When el vigilante se ubica en el tab ingresar vehiculo
+    And este en el tab carro
+    And e introduce la placa "<placa>"
+    And de click en el boton ingresar
+    Then entonces podra ver un mensaje "<mensaje>" parqueado con exito
+
+    Examples: 
+      | placa   | mensaje                                 |
+      | 777-ccc | Carro con placa 777-ccc Parqueado Exito! |
--- src/main/java/stepDefinitions/IngresarVehiculoParqueadero.java
+++ src/main/java/stepDefinitions/IngresarVehiculoParqueadero.java
@@ -27,48 +27,91 @@ public class IngresarVehiculoParqueadero {
 		controller.close();
 	}
 
+	// ingresar vehiculo tipo moto
+
 	@Given("el vigilante esta en la pagina principal del parqueadero")
 	public void el_vigilante_esta_en_la_pagina_principal_del_parqueadero() {
 		controller.manage().window().maximize();
 		controller.get("http://localhost:4200/");
 	}
-	
+
 	@When("el vigilante de click en el tab ingresar vehiculo")
 	public void el_vigilante_de_click_en_el_tab_ingresar_vehiculo() throws InterruptedException {
 		controller.findElement(By.id("mat-tab-label-0-1")).click();
 		Thread.sleep(1000);
 	}
-	
+
 	@When("este en el tab moto")
 	public void este_en_el_tab_moto() throws InterruptedException {
 		controller.findElement(By.id("mat-tab-label-1-0")).click();
 		Thread.sleep(1000);
 	}
-	
+
 	@When("^e ingrese la placa \"(.*)\"$")
 	public void e_ingrese_la_placa(String placa) throws InterruptedException {
 		controller.findElement(By.name("placa-moto")).sendKeys(placa);
 		Thread.sleep(1000);
 	}
-	
+
 	@When("^e ingrese el cilindraje \"(.*)\"$")
 	public void e_ingrese_el_cilindraje(String cilindraje) throws InterruptedException {
 		controller.findElement(By.name("cilindraje")).sendKeys(cilindraje);
 		Thread.sleep(1000);
 	}
-	
+
 	@When("el vigilante de click en el boton ingresar")
 	public void el_vigilante_de_click_en_el_boton_ingresar() throws InterruptedException {
 		controller.findElement(By.id("ingresar-vehiculo-moto-submit")).click();
 		Thread.sleep(1000);
 	}
-	
+
 	@Then("entonces el vigilante podra ver un mensaje \"(.*)\" parqueado con exito$")
 	public void entonces_el_vigilante_podra_ver_un_mensaje_parqueado_con_exito(String message) {
-	
+
 		String result = controller.findElement(By.name("ingreso-vehiculo-moto-successfull-alert")).getText();
-	
+
 		assertEquals(message, result);
-		
+
 	}
+
+	// ingresar vehiculo tipo carro
+
+	@Given("el vigilante se encuentra la pagina principal del parqueadero")
+	public void el_vigilante_se_encuentra_la_pagina_principal_del_parqueadero() {
+		controller.manage().window().maximize();
+		controller.get("http://localhost:4200/");
+	}
+
+	@When("el vigilante se ubica en el tab ingresar vehiculo")
+	public void el_vigilante_se_ubica_en_el_tab_ingresar_vehiculo() throws InterruptedException {
+		controller.findElement(By.id("mat-tab-label-0-1")).click();
+		Thread.sleep(1000);
+	}
+
+	@When("este en el tab carro")
+	public void este_en_el_tab_carro() throws InterruptedException {
+		controller.findElement(By.id("mat-tab-label-1-1")).click();
+		Thread.sleep(1000);
+	}
+
+	@When("^e introduce la placa \"(.*)\"$")
+	public void e_introduce_la_placa(String placa) throws InterruptedException {
+		controller.findElement(By.name("placa-carro")).sendKeys(placa);
+		Thread.sleep(1000);
+	}
+
+	@When("de click en el boton ingresar")
+	public void de_click_en_el_boton_ingresar() throws InterruptedException {
+		controller.findElement(By.id("ingresar-vehiculo-carro-submit")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("^entonces podra ver un mensaje \"(.*)\" parqueado con exito$")
+	public void entonces_podra_ver_un_mensaje_parqueado_con_exito(String message) {
+		
+		String result = controller.findElement(By.name("ingreso-vehiculo-carro-successfull-alert")).getText();
+
+		assertEquals(message, result);
+	}
+
 }
